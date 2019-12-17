48172f749e9dba0c88f7328f9708ab8491b1383d
Vlad Khazin <vkhazin@vkhazin.local>
2019-12-17 15:56:47 -0500

    update publish script

--- automation/publish-functions.sh
+++ automation/publish-functions.sh
@@ -2,6 +2,6 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/env-vars.sh
 
-cd ./src/
+cd ./Src/
 func azure functionapp publish $nameFunction --dotnet-cli-params -- "--configuration Release"
 cd -
