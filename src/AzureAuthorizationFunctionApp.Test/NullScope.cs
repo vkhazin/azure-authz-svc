794f1744784fba652690bf13dba04c418c6a3043
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2019-12-17 21:39:18 +0000

    adding test project

---
+++ src/AzureAuthorizationFunctionApp.Test/NullScope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AzureAuthorizationFunctionApp.Test
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        /// <summary>Prevents a default instance of the <see cref="NullScope"/> class from being created.</summary>
+        private NullScope() { }
+
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+        public void Dispose() { }
+    }
+}
